ffc65186677b3997150f884236a793a49a010e9d
Carlos Eduardo <97640110+carlosEduardo704@users.noreply.github.com>
2022-07-25 07:44:31 -0300

    25/07

---
+++ gerador_de_senhas.py
@@ -0,0 +1,17 @@
+def gerador_de_senha()
+
+escolha = input(
+    'Bem vindo ao gerador de senhas!\n'
+    'Escolha a força da senha:\n'
+    '1 - Fraca(somente numeros ou letras)\n'
+    '2 - Forte(letras e númeoros)\n'
+    '3 - Muito Forte(letras, números, letra maiúscula, caractéres especiais, tanhanho mínimo de 8 caracteres)\n'
+    '4 - Sair!\n:'
+)
+
+if escolha == '4':
+    print('Ok, Até Logo!')
+elif escolha == '1':
+
+
+
